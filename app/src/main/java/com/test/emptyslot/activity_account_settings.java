8d241b48dbf67bc121aa68edc913a18b25d5da07
hishd <hish2k15@gmail.com>
2020-01-13 01:19:49 +0530

    Realtime Location Updates

--- app/src/main/java/com/test/emptyslot/activity_account_settings.java
+++ app/src/main/java/com/test/emptyslot/activity_account_settings.java
@@ -3,12 +3,45 @@ package com.test.emptyslot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import spencerstudios.com.bungeelib.Bungee;
 
 public class activity_account_settings extends AppCompatActivity {
+
+    ImageView img_back;
+    TextView txtName;
+    TextView txtEmail;
+    TextView txtPassword;
+    TextView txtChangeName;
+    TextView txtChangeEmail;
+    TextView txtChangePassword;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+
+        img_back = findViewById(R.id.img_back);
+        txtName = findViewById(R.id.txtName);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtChangeName = findViewById(R.id.txtChangeName);
+        txtChangeEmail = findViewById(R.id.txtChangeEmail);
+        txtChangePassword = findViewById(R.id.txtChangePassword);
+        btnLogout = findViewById(R.id.btnLogout);
+
+
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                Bungee.fade(activity_account_settings.this);
+            }
+        });
     }
 }
--- app/src/main/java/com/test/emptyslot/activity_main_map_view.java
+++ app/src/main/java/com/test/emptyslot/activity_main_map_view.java
@@ -1,13 +1,24 @@
 package com.test.emptyslot;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.github.rubensousa.floatingtoolbar.FloatingToolbar;
@@ -20,6 +31,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
@@ -32,7 +44,9 @@ import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 
 import java.util.Arrays;
 
-public class activity_main_map_view extends FragmentActivity implements OnMapReadyCallback {
+import spencerstudios.com.bungeelib.Bungee;
+
+public class activity_main_map_view extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     MarkerOptions markerOptions;
@@ -47,12 +61,19 @@ public class activity_main_map_view extends FragmentActivity implements OnMapRea
     FloatingToolbar floatingToolbar;
     FloatingActionButton fab;
 
+    //Location
+    LocationManager locationManager;
+    Criteria criteria;
+    String locationProvider;
+    Location location;
+    LatLng latLng;
+    MarkerOptions myLocationMarker;
+    Marker marker;
 
-    void setUpCluster(){
-        unityPlaza = new LatLng(6.8935743, 79.8544723);
+    void setUpCluster() {
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(unityPlaza, 17.5f));
 
-        clusterManager = new ClusterManager<SlotMarkerItem>(this,mMap);
+        clusterManager = new ClusterManager<SlotMarkerItem>(this, mMap);
         mMap.setOnCameraIdleListener(clusterManager);
         mMap.setOnMarkerClickListener(clusterManager);
 
@@ -82,45 +103,7 @@ public class activity_main_map_view extends FragmentActivity implements OnMapRea
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME));
-
-        btnfocus = findViewById(R.id.btnfocus);
-        floatingToolbar = findViewById(R.id.floatingToolbar);
-        fab = findViewById(R.id.fab);
-
-
-
-        floatingToolbar.setMenu(R.menu.main);
-
-        floatingToolbar.attachFab(fab);
-
-
-        Places.initialize(getApplicationContext(),getResources().getString(R.string.maps_api_key));
-        placesClient = Places.createClient(this);
-
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 18.0f));
-            }
-
-            @Override
-            public void onError(@NonNull Status status) {
-
-            }
-        });
-
-        btnfocus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(unityPlaza, 17.5f));
-            }
-        });
+        unityPlaza = new LatLng(6.8935743, 79.8544723);
 
 
 //        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
@@ -142,6 +125,121 @@ public class activity_main_map_view extends FragmentActivity implements OnMapRea
 //            }
 //        });
 
+    }
+
+    private void setListeners() {
+        //Setting Listeners
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 18.0f));
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+
+        btnfocus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(unityPlaza, 17.5f));
+            }
+        });
+
+        floatingToolbar.setClickListener(new FloatingToolbar.ItemClickListener() {
+            @Override
+            public void onItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_my_account) {
+                    startActivity(new Intent(activity_main_map_view.this, activity_account_settings.class));
+                    Bungee.zoom(activity_main_map_view.this);
+                }
+            }
+
+            @Override
+            public void onItemLongClick(MenuItem item) {
+
+            }
+        });
+    }
+
+    private void initResources() {
+        autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG, Place.Field.NAME));
+
+        ((EditText)autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_input)).setHint("Search Destination");
+        ((EditText)autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_input)).setTextSize(14.0f);
+
+        btnfocus = findViewById(R.id.btnfocus);
+        floatingToolbar = findViewById(R.id.floatingToolbar);
+        fab = findViewById(R.id.fab);
+
+
+        floatingToolbar.setMenu(R.menu.main);
+        floatingToolbar.attachFab(fab);
+
+
+        Places.initialize(getApplicationContext(), getResources().getString(R.string.maps_api_key));
+        placesClient = Places.createClient(this);
+
+        initMyLocationMarker();
+    }
+
+    private void initMyLocationMarker() {
+        myLocationMarker = new MarkerOptions();
+        myLocationMarker.draggable(false);
+        myLocationMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.drive));
+        myLocationMarker.position(unityPlaza);
+
+        marker = mMap.addMarker(myLocationMarker);
+    }
+
+    private void initLocationUpdates() {
+
+        if (ContextCompat.checkSelfPermission(activity_main_map_view.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity_main_map_view.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(activity_main_map_view.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                ActivityCompat.requestPermissions(activity_main_map_view.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }else{
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            criteria = new Criteria();
+            locationProvider = locationManager.getBestProvider(criteria, true);
+            location = locationManager.getLastKnownLocation(locationProvider);
+
+            if(location!=null){
+                onLocationChanged(location);
+            }
+
+            locationManager.requestLocationUpdates(locationProvider,20000,0,activity_main_map_view.this);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1: {
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(activity_main_map_view.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 
 
@@ -172,6 +270,42 @@ public class activity_main_map_view extends FragmentActivity implements OnMapRea
         final CustomClusterRenderer renderer = new CustomClusterRenderer(this, mMap, clusterManager);
 
         clusterManager.setRenderer(renderer);
+
+        //Init Resources
+
+        initResources();
+
+        //Set Listeners
+
+        setListeners();
+
+        initLocationUpdates();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latLng = new LatLng(location.getLatitude(),location.getLongitude());
+
+        marker.remove();
+        myLocationMarker.position(latLng);
+        marker = mMap.addMarker(myLocationMarker);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.5f));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 
